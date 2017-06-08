1edfdc2898cc9f73db6fec9d33fb8719e522a297
Adam Cook <Adam.Cook@bbc.co.uk>
2017-06-08 16:21:10 +0100

    Initial commit.

---
+++ spec/test.js
@@ -0,0 +1,55 @@
+const stash = require('../stash')
+
+describe('Stash', () => {
+  it('should be able to set an item', () => {
+    let cache = stash()
+    cache.set('foo', 'bar')
+    expect(cache.size()).toBe(1)
+  });
+
+  it('should be able to get an item', () => {
+    let cache = stash()
+    cache.set('foo', 'foo')
+    expect(cache.get('foo')).toEqual('foo')
+  })
+
+  it('should be able to empty the cache', () => {
+    let cache = stash()
+    cache.set('foo', 'foo')
+    cache.set('bar', 'bar')
+    expect(cache.size()).toBe(2)
+    cache.empty()
+    expect(cache.size()).toBe(0)
+  })
+
+  it('should be able to set a timeout', (done) => {
+    let timeout = 1000
+    let cache = stash(timeout)
+    cache.set('foo', 'bar')
+    expect(cache.get('foo')).toBe('bar')
+    setTimeout(() => {
+      expect(cache.get('foo')).toBe(null)
+      done()
+    }, timeout)
+  })
+
+  it('should override an already set key', () => {
+    let cache = stash()
+    cache.set('foo', 'bar')
+    cache.set('foo', 'qux')
+    expect(cache.get('foo')).toEqual('qux')
+  })
+
+  it(`should not use an overidden key's timeout`, (done) => {
+    let timeout = 1000
+    let cache = stash(timeout)
+    cache.set('foo', 'bar')
+    setTimeout(() => {
+      cache.set('foo', 'qux')
+      setTimeout(() => {
+        expect(cache.get('foo')).toBe('qux')
+        done()
+      }, 500)
+    }, 500)
+  })
+});
---
+++ stash.js
@@ -0,0 +1,49 @@
+function stash (timeout) {
+  let cache = {}
+
+  function set (key, value) {
+    if (cache[key]) {
+      remove(key)
+    }
+
+    cache[key] = {}
+    cache[key].value = value
+
+    if (timeout) {
+      cache[key].timeout = setTimeout(function () {
+        remove(key)
+      }, timeout)
+    }
+  }
+
+  function get (key) {
+    if (cache[key]) {
+      return cache[key].value
+    }
+
+    return null
+  }
+
+  function remove (key) {
+    clearTimeout(cache[key].timeout)
+    cache[key] = null
+  }
+
+  function empty () {
+    for (key in cache) {
+      clearTimeout(key.timeout)
+    }
+
+    cache = {}
+  }
+
+  return {
+    set: set,
+    get: get,
+    keys: () => Object.keys(cache),
+    size: () => Object.keys(cache).length,
+    empty: empty
+  }
+}
+
+module.exports = stash
